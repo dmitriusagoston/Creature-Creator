e48eeac5ab92a1ff9095f0fee5d072a24463313b
dmitriusagoston <dmitri.agoston@gmail.com>
2024-03-07 20:51:08 -0800

    added basic creature and env class

---
+++ environment.py
@@ -0,0 +1,8 @@
+class environment:
+    def __init__(self, temp, weather, terrain, flora, prey, predators):
+        self.temp = temp
+        self.weather = weather
+        self.terrain = terrain
+        self.flora = flora
+        self.prey = prey
+        self.predators = predators
